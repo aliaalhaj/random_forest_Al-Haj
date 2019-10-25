394afe4399366a24eb186962778f0c47923ef580
Alia Al-Haj <aalhaj@bu.edu>
2019-10-25 15:31:37 -0700

    update

--- install.R
+++ install.R
@@ -1,3 +1,6 @@
-pkgs = c("tidyverse", "phyloseq", "randomForest", "ggplot2", "rfUtilities","knitr", "caret", "e1071", "klaR")
+pkgs = c("tidyverse", "randomForest", "ggplot2", "rfUtilities","knitr", "caret", "e1071", "klaR")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+  BiocManager::install("phyloseq", version = "1.28")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
